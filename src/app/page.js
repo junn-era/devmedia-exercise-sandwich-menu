d45c2fed4d64d184f24ac7887ef19bfbc3514fb0
Junn-era <junnera@gmail.com>
2024-08-11 11:04:55 -0300

    Updating Menu

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,29 @@
-
+'use client';
+import { useState } from "react";
 import styles from "./page.module.css";
+import Menu from "./components/Menu";
 
 export default function Home() {
+
+  const [menuAberto, setMenuAberto] = useState(false);
+
+  function atualizarMenu() {
+    setMenuAberto(!menuAberto)
+  }
+
   return (
-    <main className={styles.main}>
-      
+    <main className={styles.main_container}>
+
+      <header className={styles.top}>
+        <button className={styles.button} onClick={atualizarMenu}>
+          {menuAberto ? 'Fechar Menu' : 'Abrir Menu'}
+        </button>
+      </header>
+
+      {menuAberto && (
+        <Menu />
+      )}
+
     </main>
   );
 }
